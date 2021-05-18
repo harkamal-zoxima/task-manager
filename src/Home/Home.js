60904d9a944db26e753aeff12a08d27f7cca3f11
harkamal-react <harkamal.singh@itmtb.com>
2021-05-18 15:08:41 +0530

    tables json added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -12,6 +12,7 @@ import React from "react";
 import "antd/dist/antd.css";
 import './Home.css'
 import NestedTable from "./Table";
+import logo from '../Assets/jk-logo.png' 
 
 const { SubMenu } = Menu;
 
@@ -39,23 +40,22 @@ class Home extends React.Component {
               this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined
             )}
           </Button>
+
           <Menu
             defaultSelectedKeys={["1"]}
             defaultOpenKeys={["sub1"]}
             mode="inline"
-            theme="dark"
+            // theme="dark"
             inlineCollapsed={this.state.collapsed}
           >
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              Option 1
-            </Menu.Item>
+            <img className="task-img" src={logo} />
             {/* <Menu.Item key="2" icon={<DesktopOutlined />}>
               Option 2
             </Menu.Item>
             <Menu.Item key="3" icon={<ContainerOutlined />}>
               Option 3
             </Menu.Item> */}
-            <SubMenu key="sub1" icon={<MailOutlined />} title="Employee Task">
+            <SubMenu key="sub1" icon={<MailOutlined />} title="Employee Management">
               <Menu.Item key="5">Option 5</Menu.Item>
               <Menu.Item key="6">Option 6</Menu.Item>
               <Menu.Item key="7">Option 7</Menu.Item>
@@ -64,7 +64,7 @@ class Home extends React.Component {
             <SubMenu
               key="sub2"
               icon={<AppstoreOutlined />}
-              title="Task Manager"
+              title="Task Management"
             >
               <Menu.Item key="9">Option 9</Menu.Item>
               <Menu.Item key="10">Option 10</Menu.Item>
--- src/Home/Table.js
+++ src/Home/Table.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
 // import './index.css';
-import { Table, Badge, Menu, Dropdown, Space } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import { Table, Badge, Menu, Dropdown, Space } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
-const menu = (
-  <Menu>
-    <Menu.Item>Action 1</Menu.Item>
-    <Menu.Item>Action 2</Menu.Item>
-  </Menu>
-);
+const data = require("../Emp.json");
 
 function NestedTable() {
   const expandedRowRender = () => {
     const columns = [
-      { title: 'Date', dataIndex: 'date', key: 'date' },
-      { title: 'Name', dataIndex: 'name', key: 'name' },
+      { title: "Date", dataIndex: "date", key: "date" },
+      { title: "Name", dataIndex: "name", key: "name" },
       {
-        title: 'Status',
-        key: 'state',
+        title: "Status",
+        key: "state",
         render: () => (
           <span>
             <Badge status="success" />
@@ -27,16 +22,16 @@ function NestedTable() {
           </span>
         ),
       },
-      { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+      { title: "Upgrade Status", dataIndex: "upgradeNum", key: "upgradeNum" },
       {
-        title: 'Action',
-        dataIndex: 'operation',
-        key: 'operation',
+        title: "Action",
+        dataIndex: "operation",
+        key: "operation",
         render: () => (
           <Space size="middle">
             <a>Pause</a>
             <a>Stop</a>
-            <Dropdown overlay={menu}>
+            <Dropdown >
               <a>
                 More <DownOutlined />
               </a>
@@ -50,45 +45,44 @@ function NestedTable() {
     for (let i = 0; i < 3; ++i) {
       data.push({
         key: i,
-        date: '2014-12-24 23:12:00',
-        name: 'This is production name',
-        upgradeNum: 'Upgraded: 56',
+        date: "2014-12-24 23:12:00",
+        name: "This is production name",
+        upgradeNum: "Upgraded: 56",
       });
     }
     return <Table columns={columns} dataSource={data} pagination={false} />;
   };
 
   const columns = [
-    { title: 'Name', dataIndex: 'name', key: 'name' },
-    { title: 'Platform', dataIndex: 'platform', key: 'platform' },
-    { title: 'Version', dataIndex: 'version', key: 'version' },
-    { title: 'Upgraded', dataIndex: 'upgradeNum', key: 'upgradeNum' },
-    { title: 'Creator', dataIndex: 'creator', key: 'creator' },
-    { title: 'Date', dataIndex: 'createdAt', key: 'createdAt' },
-    { title: 'Action', key: 'operation', render: () => <a>Publish</a> },
+    { title: "Emp. Code", dataIndex: "empCode", key: "empCode" },
+    { title: "Name", dataIndex: "name", key: "name" },
+    { title: "Designation", dataIndex: "designation", key: "designation" },
+    { title: "Salary", dataIndex: "salary", key: "salary" },
+    { title: "Start Date", dataIndex: "startDate", key: "startDate" },
+    // { title: "Status", key: "status", dataIndex:"status" },
   ];
 
-  const data = [];
-  for (let i = 0; i < 3; ++i) {
-    data.push({
-      key: i,
-      name: 'Screem',
-      platform: 'iOS',
-      version: '10.3.4.5654',
-      upgradeNum: 500,
-      creator: 'Jack',
-      createdAt: '2014-12-24 23:12:00',
-    });
-  }
+  // const data = [];
+  // for (let i = 0; i < 3; ++i) {
+  //   data.push({
+  //     key: i,
+  //     name: 'Screem',
+  //     platform: 'iOS',
+  //     version: '10.3.4.5654',
+  //     upgradeNum: 500,
+  //     creator: 'Jack',
+  //     createdAt: '2014-12-24 23:12:00',
+  //   });
+  // }
 
   return (
     <Table
       className="components-table-demo-nested"
       columns={columns}
       expandable={{ expandedRowRender }}
-      dataSource={data}
+      dataSource={data.Data}
     />
   );
 }
 
-export default NestedTable
+export default NestedTable;
