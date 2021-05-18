3874e1e11532cf2da803f385dd4b3b214d9b1683
harkamal-react <harkamal.singh@itmtb.com>
2021-05-18 14:10:31 +0530

    table added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,4 @@
-import { Menu, Button } from 'antd';
+import { Menu, Button, Table } from "antd";
 import {
   AppstoreOutlined,
   MenuUnfoldOutlined,
@@ -7,9 +7,11 @@ import {
   DesktopOutlined,
   ContainerOutlined,
   MailOutlined,
-} from '@ant-design/icons';
-import React from 'react'
-import 'antd/dist/antd.css';
+} from "@ant-design/icons";
+import React from "react";
+import "antd/dist/antd.css";
+import './Home.css'
+import NestedTable from "./Table";
 
 const { SubMenu } = Menu;
 
@@ -26,41 +28,56 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div style={{ width: 256 }}>
-        <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
-          {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
-        </Button>
-        <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode="inline"
-          theme="dark"
-          inlineCollapsed={this.state.collapsed}
-        >
-          <Menu.Item key="1" icon={<PieChartOutlined />}>
-            Option 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<DesktopOutlined />}>
-            Option 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<ContainerOutlined />}>
-            Option 3
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-            <Menu.Item key="5">Option 5</Menu.Item>
-            <Menu.Item key="6">Option 6</Menu.Item>
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-            <Menu.Item key="9">Option 9</Menu.Item>
-            <Menu.Item key="10">Option 10</Menu.Item>
-            <SubMenu key="sub3" title="Submenu">
-              <Menu.Item key="11">Option 11</Menu.Item>
-              <Menu.Item key="12">Option 12</Menu.Item>
+      <div className="outer-div">
+        <div className="left-div">
+          <Button
+            type="primary"
+            onClick={this.toggleCollapsed}
+            style={{ marginBottom: 16 }}
+          >
+            {React.createElement(
+              this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined
+            )}
+          </Button>
+          <Menu
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            mode="inline"
+            theme="dark"
+            inlineCollapsed={this.state.collapsed}
+          >
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            {/* <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<ContainerOutlined />}>
+              Option 3
+            </Menu.Item> */}
+            <SubMenu key="sub1" icon={<MailOutlined />} title="Employee Task">
+              <Menu.Item key="5">Option 5</Menu.Item>
+              <Menu.Item key="6">Option 6</Menu.Item>
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
             </SubMenu>
-          </SubMenu>
-        </Menu>
+            <SubMenu
+              key="sub2"
+              icon={<AppstoreOutlined />}
+              title="Task Manager"
+            >
+              <Menu.Item key="9">Option 9</Menu.Item>
+              <Menu.Item key="10">Option 10</Menu.Item>
+              <SubMenu key="sub3" title="Submenu">
+                <Menu.Item key="11">Option 11</Menu.Item>
+                <Menu.Item key="12">Option 12</Menu.Item>
+              </SubMenu>
+            </SubMenu>
+          </Menu>
+        </div>
+        <div className="table-div">
+          <NestedTable />
+        </div>
       </div>
     );
   }
